835b4737b0f7dc3bf9220771bcf00d9f66e87b2d
17racos <Dracos@gmail.com>
2021-04-03 05:56:02 -0400

    adding first day 01APR2021

---
+++ project1/parser.py
@@ -0,0 +1,5 @@
+import os
+os.parh.join('usr', 'bin', 'spam')
+imyFile = ['log.txt', 'kernlog.txt']
+ifor filename in myFiles:
+	print(os.path.join('/home/dracos/projects/dailycode/project1', filename))
