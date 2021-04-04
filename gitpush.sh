a45a0022644202d8e66ef52be31cd21e38e210b0
17racos <Dracos@gmail.com>
2021-04-04 11:07:22 -0400

    adding project2 and updates on project1 and gitpush script,'04-04-2021'

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,6 @@ NOW=$(date +"%m-%d-%Y")
 echo $NOW
 git add . 
 git push -u origin main 
+git commit -m "adding project2 and updates on project1 and gitpush script,'$NOW'"
 #commitcomment = input()
-git commit -m "'adding project2 and updates on project1 and gitpush script','$NOW'"
 #git commit -m "'commitcoment','$NOW'"
